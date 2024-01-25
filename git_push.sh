86376a1a14ac2ccc7fbafb472df6d02a65c2f0bf
belkhoumali-Ahmed-Abdelmounaim <ahmed_abd_el_mounaim.belkhoumali@g.enp.edu.dz>
2024-01-25 19:26:02 +0100

    another commit

---
+++ git_push.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Add all changes to git
+git add .
+
+# Commit the changes
+git commit -m "another commit"
+
+# Set the branch name to 'main'
+git branch -M main
+
+# Add remote repository URL
+git remote add origin https://github.com/bmounim/Web-Image-Analysis-App.git
+
+# Push changes to remote repository
+git push -u origin main
