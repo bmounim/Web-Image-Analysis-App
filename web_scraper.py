c6a9f29b4ac0aa1b0518e63dc2b10e19598fe1d1
belkhoumali-Ahmed-Abdelmounaim <ahmed_abd_el_mounaim.belkhoumali@g.enp.edu.dz>
2024-01-25 21:06:17 +0100

    another commit

--- web_scraper.py
+++ web_scraper.py
@@ -27,7 +27,7 @@ class WebScraper:
 
         # Initialize the Chrome driver with the defined options
         #self.driver = webdriver.Chrome(service=Service(ChromeDriverManager(driver_version="114.0.5735.90").install()), options=self.chrome_options)
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager(version='114.0.5735.90').install()))
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager(driver_version='114.0.5735.90').install()))
 
     def handle_cookies(self, url):
         """
