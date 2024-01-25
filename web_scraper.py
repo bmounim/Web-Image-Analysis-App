395f035c54546dc1f8763615bc0308f1cdd972dc
belkhoumali-Ahmed-Abdelmounaim <ahmed_abd_el_mounaim.belkhoumali@g.enp.edu.dz>
2024-01-25 21:58:53 +0100

    another commit

--- web_scraper.py
+++ web_scraper.py
@@ -10,10 +10,88 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 
+import os
+import shutil
+
+import streamlit as st
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
+import os
+import shutil
+
+import streamlit as st
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+@st.cache_resource(show_spinner=False)
+def get_logpath():
+    return os.path.join(os.getcwd(), 'selenium.log')
+
+
+@st.cache_resource(show_spinner=False)
+def get_chromedriver_path():
+    return shutil.which('chromedriver')
+
+
+@st.cache_resource(show_spinner=False)
+def get_webdriver_options():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-features=NetworkService")
+    options.add_argument("--window-size=1920x1080")
+    options.add_argument("--disable-features=VizDisplayCompositor")
+    return options
+
+
+def get_webdriver_service(logpath):
+    service = Service(
+        executable_path=get_chromedriver_path(),
+        log_output=logpath,
+    )
+    return service
+
+
+def delete_selenium_log(logpath):
+    if os.path.exists(logpath):
+        os.remove(logpath)
+
+
+def show_selenium_log(logpath):
+    if os.path.exists(logpath):
+        with open(logpath) as f:
+            content = f.read()
+            st.code(body=content, language='log', line_numbers=True)
+    else:
+        st.warning('No log file found!')
+
+
+def run_selenium(logpath):
+    name = str()
+    with webdriver.Chrome(options=get_webdriver_options(), service=get_webdriver_service(logpath=logpath)) as driver:
+        url = "https://www.unibet.fr/sport/football/europa-league/europa-league-matchs"
+        driver.get(url)
+        xpath = '//*[@class="ui-mainview-block eventpath-wrapper"]'
+        # Wait for the element to be rendered:
+        element = WebDriverWait(driver, 10).until(lambda x: x.find_elements(by=By.XPATH, value=xpath))
+        name = element[0].get_property('attributes')[0]['name']
+    return name
 
 
 class WebScraper:
     def __init__(self):
+        logpath=get_logpath()
+        
         # Define Chrome options for headless mode
         self.chrome_options = webdriver.ChromeOptions()
         self.chrome_options.add_argument('--headless')
@@ -22,7 +100,7 @@ class WebScraper:
 
         # Initialize the Chrome driver with the defined options
         #self.driver = webdriver.Chrome(service=Service(ChromeDriverManager(driver_version="114.0.5735.90").install()), options=self.chrome_options)
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager(driver_version="114.0.5735.16").install()))
+        self.driver = webdriver.Chrome(options=self.chrome_options, service=get_webdriver_service(logpath=logpath))
 
     def handle_cookies(self, url):
         """
@@ -66,16 +144,3 @@ class WebScraper:
         Closes the WebDriver session.
         """
         self.driver.quit()
-
-# Example usage
-if __name__ == "__main__":
-    scraper = WebScraper()
-    try:
-        # Handle cookies on a specific URL
-        scraper.handle_cookies("https://www.example.com")
-        
-        # Capture a screenshot or perform other actions after handling cookies
-        screenshot_data = scraper.capture_and_return_fullpage_screenshot("https://www.example.com")
-        # Handle the screenshot data as needed
-    finally:
-        scraper.close()
