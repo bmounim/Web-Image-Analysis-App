0615580bd5acc0ca1a77d6315a838998507434ef
belkhoumali-Ahmed-Abdelmounaim <ahmed_abd_el_mounaim.belkhoumali@g.enp.edu.dz>
2024-01-31 17:02:47 +0100

    another commit

--- src/web_scraper.py
+++ src/web_scraper.py
@@ -44,6 +44,8 @@ def get_chromedriver_path():
 @st.cache_resource(show_spinner=False)
 def get_webdriver_options():
     options = Options()
+    options.add_argument('ignore-certificate-errors')
+    options.add_argument('--ignore-ssl-errors=yes')
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
@@ -93,6 +95,9 @@ class WebScraper:
         logpath=get_logpath()
         # Define Chrome options for headless mode
         self.chrome_options = webdriver.ChromeOptions()
+        self.chrome_options.add_argument('ignore-certificate-errors')
+        self.chrome_options.add_argument('--ignore-ssl-errors=yes')
+
         self.chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36")
         self.chrome_options.add_argument('--headless')
         self.chrome_options.add_argument('--no-sandbox')
