a53f5354c3c737f694777c98070f56b47b4e5e64
belkhoumali-Ahmed-Abdelmounaim <ahmed_abd_el_mounaim.belkhoumali@g.enp.edu.dz>
2024-02-01 13:02:00 +0100

    another commit

--- src/config.py
+++ src/config.py
@@ -2,6 +2,8 @@
 
 import os
 import streamlit as st
+import streamlit as st
+
 # Google Cloud settings
 #GOOGLE_PROJECT_ID = get_env_variable('GOOGLE_PROJECT_ID', "jovial-circuit-412017")
 #GOOGLE_APPLICATION_CREDENTIALS = get_env_variable('GOOGLE_APPLICATION_CREDENTIALS', "GCP_key.json")
